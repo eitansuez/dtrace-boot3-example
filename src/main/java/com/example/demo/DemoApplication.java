5dc6984c21e3aac0c01770971de7d620bdf245d7
Eitan Suez <eitan@tetrate.io>
2022-12-09 15:37:20 -0600

    another refactoring

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -38,6 +35,7 @@ class Config {
 class MyController {
 
 	private final RestTemplate restTemplate;
+
 	private final Environment environment;
 
 	public MyController(RestTemplate restTemplate, Environment environment) {
@@ -59,13 +57,11 @@ class MyController {
 	}
 
 	@GetMapping("/manual")
-	public String manualEndpoint(@RequestHeader(value="x-request-id", required=false) String requestId) throws URISyntaxException {
+	public String manualEndpoint(@RequestHeader(value="x-request-id", required=false) String requestId) {
 		String url = localHelloEndpointUrl();
 		System.out.println("calling url "+url);
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("x-request-id", requestId);
-		RequestEntity<String> request = new RequestEntity<>(headers, HttpMethod.GET, new URI(url));
+		RequestEntity<Void> request = RequestEntity.get(URI.create(url)).header("x-request-id", requestId).build();
 		ResponseEntity<String> response = restTemplate.exchange(request, String.class);
 		return response.getBody();
 	}
